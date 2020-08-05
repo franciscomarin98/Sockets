ff98c6391352650f2fac2d27cf783b7831876186
francisco-1998 <francmarinc@gmail.com>
2020-08-05 16:39:40 -0500

    Probando extensiones

--- classes/server.ts
+++ classes/server.ts
@@ -1,20 +1,18 @@
-
-import express  from "express";
-import { SERVER_PORT } from '../global/enviroment';
+import express from "express";
+import { SERVER_PORT } from "../global/enviroment";
 
 export default class Server {
-    public app: express.Application;
-    public port: number;
+  public app : express.Application;
+  public port: number;
 
-    constructor() {
-        this.app = express();
-        this.port = SERVER_PORT;
-    }
+  constructor() {
+    this.app  = express();
+    this.port = SERVER_PORT;
+  }
 
-    startServer(){
-        this.app.listen(this.port,() => {
-            console.log(`Servidor corriendo en el ${this.port}`);
-        }); 
-    }
-
+  startServer() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el ${this.port}`);
+    });
+  }
 }
