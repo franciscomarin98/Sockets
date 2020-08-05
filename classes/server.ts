c4fabb40a37cfb94eff7a2d2f852d13840c33c4d
francisco-1998 <francmarinc@gmail.com>
2020-08-05 17:48:05 -0500

    Configuracion del patron Singleton

--- classes/server.ts
+++ classes/server.ts
@@ -1,14 +1,34 @@
+import http from "http";
 import express from "express";
+import socketIO from "socket.io";
 import { SERVER_PORT } from "../global/enviroment";
 
 export default class Server {
-  public app : express.Application;
-  public port: number;
 
-  constructor() {
-    this.app  = express();
-    this.port = SERVER_PORT;
+  private static _instance: Server;
+  public app: express.Application;
+  public port: number;
+  public io: socketIO.Server;
+  private httpServer: http.Server;
+
+  private escucharSockets() {
+    console.log('Escuchando conexiones - sockets');
+    this.io.on('connection',cliente => {
+      console.log('Nuevo cliente conectado');
+    })
   }
+
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  } 
 
   startServer() {
     this.app.listen(this.port, () => {
